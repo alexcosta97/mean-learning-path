0695e78f247d025b2baa303d4da06abefc85f7fb
Alexandre Costa <alex.callum.c@gmail.com>
2018-11-16 13:19:25 +0000

    feat: add user model

--- project-1/config/mongoose.js
+++ project-1/config/mongoose.js
@@ -5,5 +5,6 @@ mongoose = require('mongoose');
 //Uses mongoose and config to connect to the database and returns the database object.
 module.exports = function(){
     var db = mongoose.connect(config.db);
+    require('../app/models/user.server.model');
     return db;
 }
