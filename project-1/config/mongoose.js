ea5be1eeff7d191249b229517000f30836841337
Alexandre Costa <alex.callum.c@gmail.com>
2018-11-16 18:17:19 +0000

    chore: set new URL parser to mongoose

--- project-1/config/mongoose.js
+++ project-1/config/mongoose.js
@@ -4,7 +4,7 @@ mongoose = require('mongoose');
 
 //Uses mongoose and config to connect to the database and returns the database object.
 module.exports = function(){
-    var db = mongoose.connect(config.db);
+    var db = mongoose.connect(config.db, {useNewUrlParser:true});
     require('../app/models/user.server.model');
     return db;
 }
