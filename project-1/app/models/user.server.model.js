733f86b854a28a5002c6bc63ba4775011f1b8391
Alexandre Costa <alex.callum.c@gmail.com>
2018-11-20 00:20:41 +0000

    feat: add default created value and website getter

--- project-1/app/models/user.server.model.js
+++ project-1/app/models/user.server.model.js
@@ -6,7 +6,25 @@ var UserSchema = new Schema({
     lastName: String,
     email: String,
     username: String,
-    password: String
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        get: function(url){
+            if(!url){
+                return url;
+            } else{
+                if(url.indexOf('http://')!== 0 && url.indexOf('https://') !== 0){
+                    url = 'https://' + url;
+                }
+                return url;
+            }
+        }
+    }
 });
+UserSchema.set('toJSON', {getters: true});
 
 mongoose.model('User', UserSchema);
