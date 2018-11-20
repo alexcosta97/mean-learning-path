63e85d96499dfde2d1aab761c526ca785cae3d47
Alexandre Costa <alex.callum.c@gmail.com>
2018-11-20 17:58:55 +0000

    feat: add validation

--- project-1/app/models/user.server.model.js
+++ project-1/app/models/user.server.model.js
@@ -6,14 +6,24 @@ var UserSchema = new Schema({
     lastName: String,
     email: {
         type: String,
-        index: true
+        index: true,
+        match: /.+\@.+\..+/
     },
     username: {
         type: String,
         trim: true,
-        unique: true
+        unique: true,
+        required: true
     },
-    password: String,
+    password: {
+        type: String,
+        validate: [
+            function(password){
+                return password.length >= 6;
+            },
+            'Password should be longer'
+        ]
+    },
     created: {
         type: Date,
         default: Date.now
@@ -30,9 +40,14 @@ var UserSchema = new Schema({
                 return url;
             }
         }
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Owner', 'User']
     }
 });
 
+//Setting fullName as a virtual field for the model
 UserSchema.virtual('fullName').get(function(){
     return this.firstName + ' ' + this.lastName;
 }).set(function(fullName){
@@ -40,6 +55,17 @@ UserSchema.virtual('fullName').get(function(){
     this.firstName = splitName[0] || '';
     this.lastName = splitName[1] || '';
 });
+
+// creating the findOneByUsername static method
+UserSchema.statics.findOneByUsername = function(username, callback){
+    this.findOne({username: new RegExp(username, 'i')}, callback);
+};
+
+//creating the instance authentication method
+UserSchema.methods.authenticate = function(password){
+    return this.password === password;
+}
+
 UserSchema.set('toJSON', {getters: true, virtuals: true});
 
 mongoose.model('User', UserSchema);
